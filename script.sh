34561a37ecd6b961e178f486a35130171f84b450
alex01 <alexschwartz01@gmail.com>
2022-12-06 13:31:52 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -5,7 +5,7 @@ wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.g
 echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
     | sudo tee /etc/apt/sources.list.d/vscodium.list
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash
-sudo apt install -y nodejs nano codium
+sudo apt install -y nano make nodejs codium
 npm config set prefix '~/.local/'
 echo "alias ll='ls -l'" >> ~/.bashrc
 echo "alias code='codium'" >> ~/.bashrc
