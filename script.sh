da0ee728a9848078edf3308ce23721e33f913cb3
alex01 <alexschwartz01@gmail.com>
2022-12-06 13:53:54 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,11 @@
-wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
+wget -qO - https://packages.microsoft.com/keys/microsoft.asc \
     | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
-echo 'deb [ signed-by=/usr/share/keyrings/vscodium-archive-keyring.gpg ] https://paulcarroty.gitlab.io/vscodium-deb-rpm-repo/debs vscodium main' \
-    | sudo tee /etc/apt/sources.list.d/vscodium.list
+    | sudo dd of=/usr/share/keyrings/packages.microsoft.gpg
+echo 'deb [ signed-by=/usr/share/keyrings/packages.microsoft.gpg ] https://packages.microsoft.com/repos/code stable main' \
+    | sudo tee /etc/apt/sources.list.d/vscode.list
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash
-sudo apt install -y nano make nodejs codium
+sudo apt install -y nano make nodejs code
 npm config set prefix '~/.local/'
 echo "alias ll='ls -l'" >> ~/.bashrc
-echo "alias code='codium'" >> ~/.bashrc
 echo 'export PATH=~/.local/bin/:$PATH' >> ~/.bashrc
 source ~/.bashrc
