aabc24720e4ff0dafd9366bd02643db67ad3dde1
alex01 <alexschwartz01@gmail.com>
2022-12-06 13:39:06 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,3 @@
-sudo apt update
 wget -qO - https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/raw/master/pub.gpg \
     | gpg --dearmor \
     | sudo dd of=/usr/share/keyrings/vscodium-archive-keyring.gpg
