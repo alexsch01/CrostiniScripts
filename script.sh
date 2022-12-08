b71a80d8491eb10e1d7db2512f4ab58fcf851989
alex01 <alexschwartz01@gmail.com>
2022-12-08 16:06:49 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ wget -qO - https://packages.microsoft.com/keys/microsoft.asc \
 echo 'deb [ signed-by=/usr/share/keyrings/packages.microsoft.gpg ] https://packages.microsoft.com/repos/code stable main' \
     | sudo tee /etc/apt/sources.list.d/vscode.list
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash
-sudo apt install -y nano make nodejs code
+sudo apt install -y nano nodejs code
 npm config set prefix '~/.local/'
 echo "alias ll='ls -l'" >> ~/.bashrc
 echo 'export PATH=~/.local/bin/:$PATH' >> ~/.bashrc
