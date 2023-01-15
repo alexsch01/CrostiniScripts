a0d3ff97c3dcb6ec2993dc6859f8b7b962cbe85b
Alex Schwartz <alexschwartz01@gmail.com>
2023-01-15 08:50:43 -0500

    Prevent duplicate entries in .bashrc

--- script.sh
+++ script.sh
@@ -1,11 +1,15 @@
 wget -qO - https://packages.microsoft.com/keys/microsoft.asc \
-    | gpg --dearmor \
-    | sudo dd of=/usr/share/keyrings/packages.microsoft.gpg
+        | gpg --dearmor \
+        | sudo dd of=/usr/share/keyrings/packages.microsoft.gpg
 echo 'deb [ signed-by=/usr/share/keyrings/packages.microsoft.gpg ] https://packages.microsoft.com/repos/code stable main' \
-    | sudo tee /etc/apt/sources.list.d/vscode.list
+        | sudo tee /etc/apt/sources.list.d/vscode.list
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash
 sudo apt install -y nano nodejs code
 npm config set prefix '~/.local/'
-echo "alias ll='ls -l'" >> ~/.bashrc
-echo 'export PATH=~/.local/bin/:$PATH' >> ~/.bashrc
+
+if ! grep -R 'export PATH=~/.local/bin/:$PATH' ~/.bashrc > /dev/null; then
+        echo 'export PATH=~/.local/bin/:$PATH' >> ~/.bashrc
+fi
+
+sed -i "s/#alias ll='ls -l'/alias ll='ls -l'/" ~/.bashrc
 source ~/.bashrc
