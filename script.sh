8f2d3cd092afb9d56da9ca9c228ee9bd60704fda
Alex Schwartz <alexschwartz01@gmail.com>
2023-09-05 12:24:23 -0400

    disable IPv6

--- script.sh
+++ script.sh
@@ -1,3 +1,6 @@
+sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
 NODE_MAJOR=18
