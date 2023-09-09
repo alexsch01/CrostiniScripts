e05628569514ad037ff5e50d778c4a20991bcb7e
Alex Schwartz <alexschwartz01@gmail.com>
2023-09-09 13:53:24 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,7 @@ if ! grep -R "net.ipv6.conf.default.disable_ipv6=1" /etc/sysctl.conf > /dev/null
         echo "net.ipv6.conf.default.disable_ipv6=1" | sudo tee -a "/etc/sysctl.conf"
 fi
 
-sudo sysctl -p
+sudo sysctl -p > /dev/null
 
 sudo mkdir -p /etc/apt/keyrings
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
