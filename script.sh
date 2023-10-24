508a2d574ddbdc74fa1b3fbc66e7440953075f57
Alex Schwartz <alexschwartz01@gmail.com>
2023-10-24 12:34:32 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,7 +7,7 @@ wget -qO - https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | s
 echo 'deb [ signed-by=/usr/share/keyrings/packages.microsoft.gpg ] https://packages.microsoft.com/repos/code stable main' | sudo tee /etc/apt/sources.list.d/vscode.list
 
 sudo apt update
-sudo apt install nano code -y
+sudo apt install nano code nodejs -y
 sudo apt purge vim --auto-remove -y
 
 sed -i "s/#alias ll='ls -l'/alias ll='ls -l'/" ~/.bashrc
